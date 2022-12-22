a242adb391ccb691ff866102245c971dc733397a
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2022-12-23 00:13:09 +0100

    verif modif token

--- routes/todo-router.js
+++ routes/todo-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const todoRouter = express.Router();
-const authToken = require("../middleware/auth-middleware")
+// const authToken = require("../middleware/auth-middleware")
 
 const createTodo = require("../controllers/add-controller");
 const update = require("../controllers/update-controller");
@@ -8,7 +8,7 @@ const todo = require("../controllers/get-one-todo");
 const getAll = require("../controllers/get-all-todos");
 const deleteTodo = require("../controllers/delete-controller");
 
-todoRouter.use(authToken)
+// todoRouter.use(authToken)
 todoRouter.post("/create", createTodo.addTodo);
 todoRouter.get("/allTodos", getAll.getAllTodos);
 todoRouter.get("/one/:id", todo.getOneTodo);
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const userRouter = express.Router();
-const authToken = require("../middleware/auth-middleware");
+// const authToken = require("../middleware/auth-middleware");
 
 
 const registerUser = require("../controllers/register-user");
+const loginUser = require("../controllers/login-user");
 const deleteUser = require("../controllers/delete-user");
 const getUsers = require("../controllers/get-users")
 const getOneUser = require("../controllers/get-one-user")
 
 //!-------------------------------- les routes qui n'ont pas besoin d'authentification ----------------------------//
-userRouter.post("/create", registerUser.register)
+userRouter.post("/register", registerUser.register)
+userRouter.post("/login", loginUser.login)
 
 //? ------------------------------- les routes AVEC les authentifications nécessaires (token)-------------------//
 
-userRouter.use(authToken); // j'utilise le middleware ici pour toute les routes qui ont besoin du token pour accéder à la route
+// TODO userRouter.use(authToken);
+// j'utilise le middleware ici pour toute les routes qui ont besoin du token pour accéder à la route
 
 
 userRouter.get("/allUsers", getUsers.getUsers);
