e7aa275d366690f8b419aa215ba2b12c95130274
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2022-12-23 00:15:10 +0100

    verification helpers

--- helpers/auth-helper.js
+++ helpers/auth-helper.js
@@ -1,12 +1,12 @@
 
 const jwt = require("jsonwebtoken")
+const ACCESS_TOKEN_SECRET = "08221543d4fcdf8070bd1d0ebb1380303671b85797d236470ca756ad2e40b7e9338f142bab45"
 
 
+function jwtTokens({ email, user_id, lastname, firstname, password }) {
 
-function jwtTokens({ email, user_id, lastname, firstname }) {
-
-    const user = { email, user_id, lastname, firstname };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const user = { email, user_id, lastname, firstname, password };
+    const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET)
     return accessToken
 }
 module.exports = { jwtTokens }
