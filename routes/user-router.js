322e1a5629a2500dae8d967851f728f15b1d0bd3
Sulayman <sirikone@hotmail.com>
2023-02-21 12:20:24 +0100

    ajout de routes pour controller get profile et l'auth token middleware

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
-// const authToken = require("../middleware/auth-middleware");
+const authToken = require("../middleware/auth-middleware");
 
 
 const registerUser = require("../controllers/register-user");
@@ -8,19 +8,19 @@ const loginUser = require("../controllers/login-user")
 const deleteUser = require("../controllers/delete-user");
 const getUsers = require("../controllers/get-users")
 const getOneUser = require("../controllers/get-one-user")
-
+const getProfile = require("../controllers/get-profile")
 //!-------------------------------- les routes qui n'ont pas besoin d'authentification ----------------------------//
 userRouter.post("/create", registerUser.register)
     .post("/login", loginUser.login)
 
 //? ------------------------------- les routes AVEC les authentifications nécessaires (token)-------------------//
 
-// TODO userRouter.use(authToken);
+userRouter.use(authToken);
 // j'utilise le middleware ici pour toute les routes qui ont besoin du token pour accéder à la route
 
 
 userRouter.get("/allUsers", getUsers.getUsers)
-
+    .get("/getProfile", getProfile.getProfile)
     .get("/:id", getOneUser.getOneUser)
     .delete("/delete/:id", deleteUser.deleteUser)
 
